20eaa2ab272ed7bb254ce6439efc8aca7fb737d3
Bahatimyk <manenomichael99@gmail.com>
2023-06-05 16:03:24 +0300

    project instatiation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as  Router, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Services from './Pages/Services/Services';
+import Contact from './Pages/Contact/Contact';
+import Profile from './Pages/Profile/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home />}></Route> 
+          <Route exact path='/About' element={<About />}></Route>
+          <Route exact path='/Services' element={<Services />}></Route>
+          <Route exact path='/Profile' element={<Profile />}></Route>
+          <Route exact path='/Contact' element={<Contact />}></Route>
+        </Routes>
+      </Router>
   );
 }
 
