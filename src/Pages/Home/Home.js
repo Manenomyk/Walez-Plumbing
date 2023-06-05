bf04815453c4fadb07ce6280ffda689d611a236b
Bahatimyk <manenomichael99@gmail.com>
2023-06-06 00:59:41 +0300

    top nav

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,22 @@
 import React from 'react'
 import './Home.css';
+import * as hom from 'react-bootstrap';
+
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+        <div className='sect1'>
+            <div className='nam1'></div>
+            <div className='nam2'>
+              <hom.Container>
+                <div className='topsec'>
+                <div className='topcontact'>Give us a call?</div>
+                <div className='topnumber'>12345678</div>
+                </div>
+              </hom.Container>
+            </div>
+        </div>
+    </div>
   )
 }
 
