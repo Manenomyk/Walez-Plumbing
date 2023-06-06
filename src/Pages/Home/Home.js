99273ec80140a4352c3ac744ca939f012c588b35
Bahatimyk <manenomichael99@gmail.com>
2023-06-06 10:01:25 +0300

    logo section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Home.css';
 import * as hom from 'react-bootstrap';
+import logo from '../../Assets/logo.jpg';
+import plum from '../../Assets/plumb.jpg';
 
 function Home() {
   return (
@@ -16,6 +18,35 @@ function Home() {
               </hom.Container>
             </div>
         </div>
+        <header>
+        <div id="nav">
+        <nav className="navigation">
+            <div className="logo">
+            {/* <img src='../Assets/log.png' alt="logo" className="img-fluid"/> */}
+            <hom.Image 
+            src={logo}
+            alt='logo'
+            className='img d-block w-100'
+            fluid
+            />
+            </div>
+            <ul>
+                <li><a href="#learnmore"> Learn more <i className="fas fa-caret-right"></i></a></li>
+                <li><a href="/login ">Customer <i className="fas fa-caret-right"></i></a></li>
+                <li><a href="/techlogin">Technician <i className="fas fa-caret-right"></i></a></li>
+                <li><a href="#0794636596"><i className="fas fa-phone-alt"></i> 0794636596</a></li>
+            </ul>
+        </nav>
+        </div>
+    </header>
+    <div className='homeimage'>
+      <hom.Image 
+      src='plum'
+      alt='home picture'
+      className='d-block w-100'
+      fluid
+      />
+    </div>
     </div>
   )
 }
