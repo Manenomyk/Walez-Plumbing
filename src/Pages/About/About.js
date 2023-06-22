94e8b49d03981ea16c6516e5d67da291219bf782
Bahatimyk <manenomichael99@gmail.com>
2023-06-22 14:19:58 +0300

    about infomation

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import './About.css';
-import * as abou from 'react-bootstrap';
+import React from "react";
+import "./About.css";
+import * as abou from "react-bootstrap";
 import { BsTelephoneFill } from "react-icons/bs";
 import logo from "../../Assets/logo.jpg";
 import abo from "../../Assets/abou.jpg";
 import owner from "../../Assets/wallace.jpeg";
 import * as rev from "react-reveal";
 import log from "../../Assets/log.png";
-
 
 function About() {
   return (
@@ -61,31 +60,57 @@ function About() {
       </header>
       <div className="homeimage1">
         <rev.Zoom delay={1000}>
-        <div className="overlay">
+          <div className="overlay">
             <h2>Having any inquiries about us?</h2>
             <p>Get to know us better</p>
             <div className="btnbody">
               <a className="linky" href="#contact">
-              <div className="buton">Reach out</div>
+                <div className="buton">Reach out</div>
               </a>
             </div>
           </div>
         </rev.Zoom>
       </div>
 
-    <div className='aboutinfo'>
-      <abou.Container>
-        <abou.Row>
-          <abou.Col lg={5} className='mx-auto'>
-
-          </abou.Col>
-          <abou.Col lg={5} className='mx-auto'>
-            
-          </abou.Col>
-        </abou.Row>
-      </abou.Container>
-    </div>
-
+      <div className="aboutinfo">
+        <abou.Container>
+          <abou.Row>
+            <abou.Col lg={5} className="mx-auto">
+              <div className="abouinfo">
+                <p className="get">Get to know us</p>
+                <p className="us">About us</p>
+                <p>
+                  Walez Plumbing and renovations company was started on 23rd of
+                  September 2021 and since then we pride in ourselves for
+                  quality and efficiency in service delivery. Our Catch for the
+                  industry is in customer satisfaction through Proffesionalism.
+                </p>
+              </div>
+            </abou.Col>
+            <abou.Col lg={5} className="mx-auto">
+              <abou.Row>
+                <abou.Col lg={5} className="mx-auto">
+                  <div>
+                    <abou.Image
+                      className="walace"
+                      src={owner}
+                      fluid
+                      alt="CEO Image"
+                    />
+                  </div>
+                </abou.Col>
+                <abou.Col lg={5} className="my-auto">
+                  <div>
+                    <p>Meet the CEO</p>
+                    <p className="name"><b>Walter Anyika Musoka</b></p>
+                    <div className="buton">Lets Talk</div>
+                  </div>
+                </abou.Col>
+              </abou.Row>
+            </abou.Col>
+          </abou.Row>
+        </abou.Container>
+      </div>
 
       <div className="contact">
         <a id="contact"></a>
@@ -96,7 +121,8 @@ function About() {
                 <h3>Contact us</h3>
                 <ul>
                   <li>
-                    <i className="fas fa-map-marker-alt"></i>Along macharia Road King's Place
+                    <i className="fas fa-map-marker-alt"></i>Along macharia Road
+                    King's Place
                   </li>
                   <li>
                     <i className="fas fa-phone-alt"></i>0714582808/0796921877
@@ -132,13 +158,13 @@ function About() {
             <abou.Col className="mx-auto" lg={4}>
               <div className="footer-links">
                 <div className="footer-logo">
-                <rev.Zoom delay={1000}>
-                  <abou.Image
-                    src={log}
-                    className="d-block w-100"
-                    fluid
-                    alt="logo"
-                  />
+                  <rev.Zoom delay={1000}>
+                    <abou.Image
+                      src={log}
+                      className="d-block w-100"
+                      fluid
+                      alt="logo"
+                    />
                   </rev.Zoom>
                 </div>
                 <ul>
@@ -211,7 +237,7 @@ function About() {
         </p>
       </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
